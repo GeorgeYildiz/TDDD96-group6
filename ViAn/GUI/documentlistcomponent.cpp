a04f46b4e35d9c4eb09353f1b10654224ef4c8b6
Fleskvegg <patrik.lundgren@outlook.com>
2017-05-02 08:40:11 +0200

    moved function

--- ViAn/GUI/documentlistcomponent.cpp
+++ ViAn/GUI/documentlistcomponent.cpp
@@ -3,6 +3,18 @@
 DocumentListComponent::DocumentListComponent()
 {
 }
+
+/**
+ * @brief MainWindow::on_document_list_itemClicked
+ * Invoked when an item in the bookmark view has been clicked.
+ * @param item The bookmark that has been clicked.
+ */
+void MainWindow::on_document_list_itemClicked(QListWidgetItem *item) {
+    BookmarkItem* bookmark = (BookmarkItem*) item;
+    emit set_playback_frame(bookmark->get_frame_number());
+    set_status_bar("Jump to frame: " + to_string(bookmark->get_frame_number()) + ".");
+}
+
 /**
  * @brief MainWindow::on_bookmark_button_clicked
  * Button to add a bookmark to the bookmark view.
